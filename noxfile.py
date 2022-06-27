beb7da4c59eaf565e4813763ddf0aa070fe75d4d
tom65536 <48080661+tom65536@users.noreply.github.com>
2022-06-27 23:12:24 +0200

    Adds basic build process

--- noxfile.py
+++ noxfile.py
@@ -4,9 +4,78 @@ Session definitions for nox.
 Run ``nox -l`` to obtain a list of all sessions.
 Run ``nox -s <session_name>`` to run a specific session.
 """
+import multiprocessing
+import pathlib
+import shutil
+import subprocess
+import typing
+
 import nox
 
+
+
+STATIC_DIRECTORY: pathlib.Path = pathlib.Path('.') / 'static'
+SITE_DIRECTORY: pathlib.Path = pathlib.Path('.') / 'target' / 'site'
+DIST_DIRECTORY: pathlib.Path = pathlib.Path('.') / 'target' / 'dist'
+
+def run_python_module(
+        ctx: nox.Session,
+        module_name: str,
+        *args: typing.Any,
+) -> None:
+    """Run a Python module with the given arguments."""
+    python_command: str = 'python'
+    python_args: typing.List[str] = []
+    python_args += ['-m', module_name]
+    python_args += map(str, args)
+    if ctx is None:
+        subprocess.run([python_command] + python_args)
+    else:
+        ctx.run(python_command, *python_args)
+
+def do_build_wheel(ctx: nox.Session) -> None:
+    """Build a wheel package."""
+    ctx.install('build')
+    output_directory: pathlib.Path = DIST_DIRECTORY
+    output_directory.mkdir(parents = True, exist_ok = True)
+    run_python_module(ctx, 'build', '-o', output_directory)
+
 @nox.session
-def run(ctx: nox.Session) -> None:
+def run_web(ctx: nox.Session) -> None:
     """Run the web page using the Python http server."""
-    ctx.run('python', 'scripts/serve_and_browse.py', 'static/index.html')
+    port: int = 8000
+    directory: pathlib.Path = SITE_DIRECTORY
+
+    httpd = multiprocessing.Process(
+            target=run_python_module,
+            args=(None, 'http.server', '--directory', directory, port),
+    )
+    try:
+        httpd.start()
+        run_python_module(None, 'webbrowser', '-t', f'http://localhost:{port}')
+    except (KeyboardInterrupt, SystemExit):
+        pass
+    finally:
+        httpd.join()
+    #run_python_module(ctx, 'http.server', '--directory', directory, port)
+
+@nox.session
+def build_web(ctx: nox.Session) -> None:
+    """Prepare target directory for running the web site."""
+    do_build_wheel(ctx)
+    
+    SITE_DIRECTORY.mkdir(parents = True, exist_ok = True)
+
+    for file_path in DIST_DIRECTORY.glob('steps-*.whl'):
+        ctx.log(f'Copying {file_path}')
+        shutil.copy(file_path, SITE_DIRECTORY / file_path.name)
+
+    for file_path in STATIC_DIRECTORY.glob('*'):
+        ctx.log(f'Copying {file_path}')
+        shutil.copy(file_path, SITE_DIRECTORY / file_path.name)
+
+@nox.session
+def build_wheel(ctx: nox.Session) -> None:
+    """Build a Pyodide package."""
+    do_build_wheel(ctx)
+
---
+++ src/steps/__init__.py
@@ -0,0 +1,6 @@
+"""STEPS - Statically Typed Educational Programming Language."""
+
+__version__: str = '0.1.0'
+
+from . import func
+from . import other
